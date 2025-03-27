67b3846b45c3da21b4c2ae48e040d6474874e07b
Salman.S <syamsalman0@gmail.com>
2025-03-27 17:38:04 +0530

    added helloworld class

---
+++ src/main/java/com/example/keycloak/helloword/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.keycloak.helloword;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+	@GetMapping("/hello-world")
+	public String getHelloWorld() {
+		return "Hello World";
+	}
+}
